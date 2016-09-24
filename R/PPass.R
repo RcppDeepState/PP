c549855a1ad23530974e27c52b8bcf4311b2937e
Manuel Reif <manuel.reif@gmail.com>
2016-09-24 15:15:02 +0200

    PPass - 3 parts

--- R/PPass.R
+++ R/PPass.R
@@ -1,3 +1,35 @@
 #### PPass function - brings it all together
 
 
+PPass <- function(respdf, mod=c("1PL","2PL","3PL","4PL","GPCM","MIXED"), ...)
+{
+
+  
+
+if(mod %in% c("1PL","2PL","3PL","4PL"))
+  {
+    
+    
+  } else if(mod %in% c("GPCM"))
+    {
+      
+    } else { # mixed
+      
+      
+      
+            }
+  
+  
+pointi <- list(...) 
+match.arg(arg, choices, several.ok = FALSE)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
