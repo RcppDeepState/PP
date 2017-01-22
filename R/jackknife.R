b5161e841f650631b41cff416bbe9c644a535f0b
Jan Steinfeld <jan.d.steinfeld@gmail.com>
2017-01-22 14:30:33 +0100

    wurden immer komsiche files gespeichert, sollte nun auch passen

--- R/jackknife.R
+++ R/jackknife.R
@@ -4,13 +4,13 @@ jackknife <- function(data,pp,fit){
   beta <- pp$ipar$thres[2,]
   if(fit=="lz"){
     for(i in 1:ncol(data)){
-  capture.output(pp.lz <- PP_4pl(respm=data[,-i],thres=pp$ipar$thres[2,][-i], lowerA=pp$ipar$lowerA[-i], slopes=pp$ipar$slopes[-i], upperA=pp$ipar$upperA[-i],type=pp$type, mu=pp$ipar$mu[-i], sigma2=sqrt(pp$ipar$sigma2[-i])), file='NULL')
+  capture.output(pp.lz <- PP_4pl(respm=data[,-i],thres=pp$ipar$thres[2,][-i], lowerA=pp$ipar$lowerA[-i], slopes=pp$ipar$slopes[-i], upperA=pp$ipar$upperA[-i],type=pp$type, mu=pp$ipar$mu[-i], sigma2=sqrt(pp$ipar$sigma2[-i])))
       out[,i] <- PP:::lz(data=data[,-i], thetas=pp.lz$resPP$resPP[,"estimate"], betas=pp.lz$ipar$thres[2,], lowerAs=pp.lz$ipar$lowerA, slopes=pp.lz$ipar$slopes, higherAs=pp.lz$ipar$upperA)[,fit]
     }
   }
   if(fit=="lzstar"){
     for(i in 1:ncol(data)){
-      capture.output(pp.ls <- PP_4pl(respm=data[,-i],thres=pp$ipar$thres[2,][-i], lowerA=pp$ipar$lowerA[-i], slopes=pp$ipar$slopes[-i], upperA=pp$ipar$upperA[-i],type=pp$type, mu=pp$ipar$mu[-i], sigma2=sqrt(pp$ipar$sigma2[-i])), file='NULL')
+      capture.output(pp.ls <- PP_4pl(respm=data[,-i],thres=pp$ipar$thres[2,][-i], lowerA=pp$ipar$lowerA[-i], slopes=pp$ipar$slopes[-i], upperA=pp$ipar$upperA[-i],type=pp$type, mu=pp$ipar$mu[-i], sigma2=sqrt(pp$ipar$sigma2[-i])))
       out[,i] <- PP:::lzstar(data=data[,-i], thetas=pp.ls$resPP$resPP[,"estimate"], betas=pp.ls$ipar$thres[2,], lowerAs=pp.ls$ipar$lowerA, slopes=pp.ls$ipar$slopes, higherAs=pp.ls$ipar$upperA,method=pp.ls$type, mu=pp.ls$ipar$mu, sigma=sqrt(pp.ls$ipar$sigma2))
     }
   }
